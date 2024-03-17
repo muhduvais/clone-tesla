5ff0c1356f088e0e7d766a3c91aeb845649573fe
uvais <162714493+muhduvais@users.noreply.github.com>
2024-03-18 02:03:00 +0530

    Hover effect on header

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function showContainer() {
     const header = document.querySelector('.hero-container header');
-    const link = document.querySelector('ul.nav-links li');
+    const links = document.querySelectorAll('ul.nav-links li');
+    const linksa = document.querySelectorAll('ul.nav-links li a');
 
-    link.addEventListener('mouseover', function() {
-        header.style.backgroundColor = '#fff'; // Replace '#fff' with your desired color
-    });
+    links.forEach(function(link) {
+        link.addEventListener('mouseover', function() {
+            header.style.backgroundColor = '#fff'; // Replace '#fff' with your desired color
+        });
+    
+        link.addEventListener('mouseout', function() {
+            header.style.backgroundColor = ''; // Reset the background color when mouse is out
+        });
+    })
 
-    link.addEventListener('mouseout', function() {
-        header.style.backgroundColor = ''; // Reset the background color when mouse is out
-    });
+    linksa.forEach(function(linka) {
+        linka.addEventListener('mouseover', function() {
+            linka.style.color = '#000'; // Replace '#fff' with your desired color
+        });
+    
+        linka.addEventListener('mouseout', function() {
+            linka.style.color = '#fff'; // Reset the background color when mouse is out
+        });
+    })
 }
+
+showContainer();
